a8a71c0058214b1921d82c0cff4702d2b3b3ad25
Blas Gabriel <blasgabriel200@gmail.com>
2024-12-18 14:35:38 -0300

    unoptimized: true,

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
     basePath: "/portafolio2",
     output: "export",  // <=== enables static exports
     reactStrictMode: true,
+    images: {
+        unoptimized: true,
+      },
 };
 
 export default nextConfig;
